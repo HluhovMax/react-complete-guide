1d4e7ff72393829ec6d96ea2981645fa3731f15f
Maksym Hlukhov <maksym.hlukhov@bitvoodoo.ch>
2020-10-30 09:07:19 +0200

    added state, setState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+    state = {
+        persons: [
+            {name: 'Max', age: 22},
+            {name: 'Lilya', age: 22},
+            {name: 'Mettew', age: 1}
+        ]
+    }
+
+    switchNameHandler = () => {
+        // console.log('was clicked')
+        // DONT DO THIS this.state.persons[0].name = 'Maximilian';
+        this.setState({
+            persons: [
+                {name: 'Maximilian', age: 22},
+                {name: 'Lilya', age: 23},
+                {name: 'Mettew', age: 1}
+            ]
+        })
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
             </div>
         );
     }
