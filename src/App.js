3ac13a71e7e7cacbb1609dfdd83369aac3afbb0c
Maksym Hlukhov <maksym.hlukhov@bitvoodoo.ch>
2020-11-04 21:20:38 +0200

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
+const app = props => {
 
-    state = {
+    const [personsState, setPersonsState] = useState({
         persons: [
             {name: 'Max', age: 22},
             {name: 'Lilya', age: 22},
             {name: 'Mettew', age: 1}
         ]
-    }
+    });
 
-    switchNameHandler = () => {
-        // console.log('was clicked')
-        // DONT DO THIS this.state.persons[0].name = 'Maximilian';
-        this.setState({
+    const switchNameHandler = () => {
+        setPersonsState({
             persons: [
                 {name: 'Maximilian', age: 22},
                 {name: 'Lilya', age: 23},
@@ -24,16 +22,14 @@ class App extends Component {
         })
     };
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p>This is really working!</p>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Racing</Person>
+        </div>
+    );
 }
 
-export default App;
+export default app;
